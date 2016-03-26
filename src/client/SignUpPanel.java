41bc2e4db73113c640f7dabd0e9496bb6328557e
Jeremy Aftem <aftem@usc.edu>
2016-03-25 21:42:19 -0700

    fixes

--- src/client/SignUpPanel.java
+++ src/client/SignUpPanel.java
@@ -2,6 +2,7 @@
 package client;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -22,7 +23,7 @@ public class SignUpPanel extends JPanel {
 	
 	private Image bg;
 	private JLabel bgLabel;
-	private HubTextField usernameTextField;
+	private HubTextField usernameTextField, fnameTextField, lnameTextField;
 	private HubPassField passwordTextField, confirmPasswordTextField;
 	private HubButton signupButton;
 	private JPanel centerPanel, buttonBox;
@@ -40,6 +41,8 @@ public class SignUpPanel extends JPanel {
 		usernameTextField = new HubTextField("Email");
 		passwordTextField = new HubPassField(Constants.PASSWORD_TEXT);
 		confirmPasswordTextField = new HubPassField("Confirm Password");
+		fnameTextField = new HubTextField("First Name");
+		lnameTextField = new HubTextField("Last Name");
 		signupButton = new HubButton(Constants.SIGNUP_TEXT, ThemeColors.SIGNUP_COLOR, ThemeColors.SIGNUP_HIGHLIGHT_COLOR);
 		centerPanel = new JPanel();		
 		buttonBox = new JPanel();
@@ -48,30 +51,35 @@ public class SignUpPanel extends JPanel {
 	private void createGUI() {
 		bgLabel.setLayout(new GridBagLayout());
 		/* Center Panel is a panel located vertically and horizontally in the center of the frame */
-		centerPanel.setPreferredSize(new Dimension(310,350));
+		centerPanel.setPreferredSize(new Dimension(310,460));
 		centerPanel.setLayout(null);
 		/* The components inside the center panel are placed by pixels,
 		 * OK because user can't resize smaller than 1100x699 */
-		usernameTextField.setBounds(0, 115, 310, 50);
-		passwordTextField.setBounds(0, 173, 310, 50);
-		confirmPasswordTextField.setBounds(0, 231, 310, 50);
+		fnameTextField.setBounds(0, 115, 150, 50);
+		lnameTextField.setBounds(160, 115, 150, 50);
+		usernameTextField.setBounds(0, 173, 310, 50);
+		passwordTextField.setBounds(0, 231, 310, 50);
+		confirmPasswordTextField.setBounds(0, 289, 310, 50);
 		Image img = ImageLibrary.getScaledImage(ImageLibrary.getImage(Constants.SPLASH_LOGO), 310, 85);
 		JLabel logoLabel = new JLabel(new ImageIcon(img));
 		logoLabel.setBounds(0, 0, 310, 85);
 		/* buttonBox: Container for buttons login and sign out */
-		buttonBox.setLayout(null);
-		buttonBox.setBounds(0, 290, 310, 50);
-		signupButton.setBounds(163,0,147,50);
+		buttonBox.setLayout(new FlowLayout());
+		buttonBox.setBounds(0, 350, 310, 60);
+		signupButton.setPreferredSize(new Dimension(310, 50));
+		//signupButton.setBounds(163,0,147,50);
 		buttonBox.add(signupButton);
-		buttonBox.setOpaque(false);
 		/* Add all the components to the center panel */
 		centerPanel.add(logoLabel);
+		centerPanel.add(fnameTextField);
+		centerPanel.add(lnameTextField);
 		centerPanel.add(usernameTextField);
 		centerPanel.add(passwordTextField);
 		centerPanel.add(confirmPasswordTextField);
 		centerPanel.add(buttonBox);
 		/* Transparent panel */
 		centerPanel.setOpaque(false);
+		buttonBox.setOpaque(false);
 		/* Add the centerPanel to the center (vertically and horizontally) of the JLabel */
 		bgLabel.add(centerPanel, new GridBagConstraints());
 		add(bgLabel);
